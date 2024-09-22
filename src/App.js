6fc713d0286ed8dc66c00e3b99b385495a8b5530
winshine0326 <shane10340@gmail.com>
2024-09-22 15:22:38 +0900

    feat: 영화 이미지,제목,연도,설명 html

--- src/App.js
+++ src/App.js
@@ -4,32 +4,45 @@ import Movie from './Movie';
 
 class App extends React.Component {
   state = {
-    isLoading : true,
-    movies : [],
+    isLoading: true,
+    movies: [],
   }
   getMovies = async () => {
     const {
       data: {
-        data : {movies},
+        data: { movies },
       }, //구조분해 할당
     } = await axios.get("https://yts-proxy.now.sh/list_movies.json?sort_by=rating"); // 평점 내림차순 정렬
-    this.setState({movies, isLoading : false}) // 객체의 키와 대입할 변수의 이름이 같음으로 축약 ({movies : movies})
+    this.setState({ movies, isLoading: false }) // 객체의 키와 대입할 변수의 이름이 같음으로 축약 ({movies : movies})
   }
-  componentDidMount(){
+  componentDidMount() {
     this.getMovies();
   }
-  render(){
+  render() {
     const { isLoading, movies } = this.state;
-    return <div>{isLoading ? 'Loading...' : movies.map((movie)=>{
-      return <Movie
-      key={movie.id}
-      id={movie.id}
-      year={movie.year}
-      title={movie.title}
-      summary={movie.summary}
-      poster={movie.medium_cover_image}
-      />;
-    })}</div>;
+    return (
+      <section class="container">
+        {isLoading ? (
+          <div class="loader">
+            <span class="loader__text">Loading...</span>
+          </div>
+        ):(
+          <div class="movies">
+            {movies.map(movie => (
+              <Movie
+                key={movie.id}
+                id={movie.id}
+                year={movie.year}
+                title={movie.title}
+                summary={movie.summary}
+                poster={movie.medium_cover_image}
+              />
+            ))}
+          </div>
+        )
+      }
+      </section>
+    )
   }
 }
 export default App;
--- src/Movie.js
+++ src/Movie.js
@@ -1,16 +1,24 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-function Movie({id,title,year,summary,poster}){
-    return <h4>{title}</h4>;
+function Movie({ title, year, summary, poster }) {
+    return (
+        <div class="movie">
+            <img src={poster} alt={title} title={title}/>
+            <div class="movie__data">
+                <h3 class="movie__title">{title}</h3>
+                <h5 class="movie__year">{year}</h5>
+                <p class="movie__summary">{summary}</p>
+            </div>
+        </div>
+    );
 }
 
 Movie.propTypes = {
-    id:PropTypes.number.isRequired,
-    year:PropTypes.number.isRequired,
-    title:PropTypes.string.isRequired,
-    summary:PropTypes.string.isRequired,
-    poster:PropTypes.string.isRequired,
+    year: PropTypes.number.isRequired,
+    title: PropTypes.string.isRequired,
+    summary: PropTypes.string.isRequired,
+    poster: PropTypes.string.isRequired,
 };
 
 export default Movie;
