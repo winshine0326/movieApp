44d35029d89feb6b6ba4b0e92f25f61f06b37b43
winshine0326 <shane10340@gmail.com>
2024-09-22 14:46:19 +0900

    docs: Movie 컴포넌트 파일 생성

---
+++ src/Movie.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+function Movie(){
+    return <h1></h1>;
+}
+
+Movie.propTypes = {
+    id:PropTypes.number.isRequired,
+    year:PropTypes.number,isRequired,
+    title:PropTypes.string,isRequired,
+    summary:PropTypes.string.isRequired,
+    poster:PropTypes.string.isRequired,
+};
+
+export default Movie;
