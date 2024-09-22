f6bcb086e446e1f7bbc40e143d2d96c0594259ae
winshine0326 <shane10340@gmail.com>
2024-09-22 14:57:07 +0900

    feat: 영화 목록 화면에 띄우기

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import axios from 'axios';
+import Movie from './Movie';
 
 class App extends React.Component {
   state = {
@@ -11,15 +12,24 @@ class App extends React.Component {
       data: {
         data : {movies},
       }, //구조분해 할당
-    } = await axios.get("https://yts-proxy.now.sh/list_movies.json");
+    } = await axios.get("https://yts-proxy.now.sh/list_movies.json?sort_by=rating"); // 평점 내림차순 정렬
     this.setState({movies, isLoading : false}) // 객체의 키와 대입할 변수의 이름이 같음으로 축약 ({movies : movies})
   }
   componentDidMount(){
     this.getMovies();
   }
   render(){
-    const { isLoading } = this.state;
-    return <div>{isLoading ? 'Loading...' : 'We are ready.'}</div>;
+    const { isLoading, movies } = this.state;
+    return <div>{isLoading ? 'Loading...' : movies.map((movie)=>{
+      return <Movie
+      key={movie.id}
+      id={movie.id}
+      year={movie.year}
+      title={movie.title}
+      summary={movie.summary}
+      poster={movie.medium_cover_image}
+      />;
+    })}</div>;
   }
 }
 export default App;
--- src/Movie.js
+++ src/Movie.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-function Movie(){
-    return <h1></h1>;
+function Movie({id,title,year,summary,poster}){
+    return <h4>{title}</h4>;
 }
 
 Movie.propTypes = {
     id:PropTypes.number.isRequired,
-    year:PropTypes.number,isRequired,
-    title:PropTypes.string,isRequired,
+    year:PropTypes.number.isRequired,
+    title:PropTypes.string.isRequired,
     summary:PropTypes.string.isRequired,
     poster:PropTypes.string.isRequired,
 };
