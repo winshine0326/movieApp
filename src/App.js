2a7100311146983b8b7079beaa3d85d600b6c0cc
winshine0326 <shane10340@gmail.com>
2024-09-22 23:53:28 +0900

    feat: 네비게이션 기능 추가

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
-import './App.css'
-import { HashRouter, Routes, Route } from 'react-router-dom'
-import About from './routes/About'
-import Home from './routes/Home'
+import './App.css';
+import { HashRouter, Routes, Route } from 'react-router-dom';
+import About from './routes/About';
+import Home from './routes/Home';
+import Navigation from './components/Navigation';
 
 function App() {
   return (
     <HashRouter>
+      <Navigation/>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
