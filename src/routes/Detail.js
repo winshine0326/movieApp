eaa9f447a625655bd04911fa12bb4e59aea16762
winshine0326 <shane10340@gmail.com>
2024-09-23 01:04:11 +0900

    fix: 리다이렉트 기능 수정

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,18 +1,25 @@
-import React,{ useEffect } from 'react';
+import React, { useEffect } from 'react';
 import { useLocation } from "react-router-dom";
 import { useNavigate } from "react-router-dom";
 
-function Detail(){
+function Detail() {
     const location = useLocation();
     const navigate = useNavigate();
     console.log(location);
-    useEffect(() => {
-        if (location.key === "default") {
-            navigate("/"); 
-        }
-    }, [location, navigate]);
 
-    return <span>hello</span>;
+    useEffect(() => { // 컴포넌트가 렌더링 된 이후 페이지 이동
+        if (location.key === "default") {
+            navigate("/");
+        }
+    }, [location, navigate]); // navigate가 변경될 때만 실행
+
+    if (location.key != "default") {
+        return (
+            <div>
+                <span>{location.state.props.title}</span>
+            </div>
+        );
+    }
 }
 
 export default Detail;
