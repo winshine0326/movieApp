a24ff5f350caaa6d5de6a7eec09e5550ce2b56fa
winshine0326 <shane10340@gmail.com>
2024-09-23 00:24:00 +0900

    feat: 영화 상세정보 컴포넌트+라우터

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { HashRouter, Routes, Route } from 'react-router-dom';
 import About from './routes/About';
 import Home from './routes/Home';
 import Navigation from './components/Navigation';
+import Detail from './routes/Detail'
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
+        <Route path="/movie-detail" element={<Detail />}/>
       </Routes>
     </HashRouter>
   );
