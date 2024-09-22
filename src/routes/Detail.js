dff17132745f01f2b0ab206e129355091c54ca38
winshine0326 <shane10340@gmail.com>
2024-09-23 00:54:57 +0900

    feat: 리다이렉트 기능 추가

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,9 +1,17 @@
-import React from 'react';
+import React,{ useEffect } from 'react';
 import { useLocation } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 function Detail(){
     const location = useLocation();
+    const navigate = useNavigate();
     console.log(location);
+    useEffect(() => {
+        if (location.key === "default") {
+            navigate("/"); 
+        }
+    }, [location, navigate]);
+
     return <span>hello</span>;
 }
 
