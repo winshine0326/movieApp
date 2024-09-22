db387fece27d4419f1fe9ee46b8023e513981d85
winshine0326 <shane10340@gmail.com>
2024-09-22 23:41:50 +0900

    feat: about 라우터 추가

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
 import React from "react";
-import Home from './routes/Home';
 import './App.css'
+import { HashRouter, Routes, Route } from 'react-router-dom'
+import About from './routes/About'
+import Home from './routes/Home'
 
-function App(){
-  return <Home />;
+function App() {
+  return (
+    <HashRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </HashRouter>
+  );
 }
 export default App;
