335d39682abb894636fecb3e9271e9519d50291e
winshine0326 <shane10340@gmail.com>
2024-09-22 23:07:26 +0900

    docs: 파일 정리

--- src/App.js
+++ src/App.js
@@ -1,50 +1,8 @@
 import React from "react";
-import axios from 'axios';
-import Movie from './Movie';
+import Home from './routes/Home';
 import './App.css'
 
-class App extends React.Component {
-  state = {
-    isLoading: true,
-    movies: [],
-  }
-  getMovies = async () => {
-    const {
-      data: {
-        data: { movies },
-      }, //구조분해 할당
-    } = await axios.get("https://yts-proxy.now.sh/list_movies.json?sort_by=rating"); // 평점 내림차순 정렬
-    this.setState({ movies, isLoading: false }) // 객체의 키와 대입할 변수의 이름이 같음으로 축약 ({movies : movies})
-  }
-  componentDidMount() {
-    this.getMovies();
-  }
-  render() {
-    const { isLoading, movies } = this.state;
-    return (
-      <section className="container">
-        {isLoading ? (
-          <div className="loader">
-            <span className="loader__text">Loading...</span>
-          </div>
-        ):(
-          <div className="movies">
-            {movies.map(movie => (
-              <Movie
-                key={movie.id}
-                id={movie.id}
-                year={movie.year}
-                title={movie.title}
-                summary={movie.summary}
-                poster={movie.medium_cover_image}
-                genres={movie.genres}
-              />
-            ))}
-          </div>
-        )
-      }
-      </section>
-    )
-  }
+function App(){
+  return <Home />;
 }
 export default App;
